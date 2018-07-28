90523c47aa1d99649bb51bf7acd9a76e1614614f
Yoshihito Hanada <hanada@gmail.com>
2018-07-28 16:46:28 +0900

    added codes sya-kyo

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += transform.forward * 0.1f;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position -= transform.forward * 0.1f;
+        }	
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collision");
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Unity: break out game sample
+
+ref [今日からはじめるUnity](https://qiita.com/nmxi/items/7950fb12ef925efa276d)
+
